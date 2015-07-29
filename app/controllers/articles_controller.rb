8a440d4ca96107bd3c99f164b519036834f5b42b
Mashrur Hossain <railsudemy@gmail.com>
2015-07-29 19:58:13 +0000

    Stylize devise messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_filter :set_article, only: [:show, :edit, :update, :destroy]
   def index
     @articles = Article.all
