088a40c5cd7563d562e9ad8d1ff67e7d2a8ab056
Mashrur Hossain <railsudemy@gmail.com>
2015-07-30 15:39:02 +0000

    Restrict new edit and delete options for signed in users and owners

--- spec/features/listing_articles_spec.rb
+++ spec/features/listing_articles_spec.rb
@@ -16,5 +16,6 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_content(@article2.body)
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
   end 
 end
--- spec/features/showing_article_spec.rb
+++ spec/features/showing_article_spec.rb
@@ -3,7 +3,45 @@ require "rails_helper"
 RSpec.feature "Showing an Article" do
   
   before do 
-    @article = Article.create(title: "The first article", body: "Body of first article")
+    @john = User.create(email: "john@example.com", password: "password")
+    @fred = User.create(email: "fred@example.com", password: "password")
+    @article = Article.create(title: "The first article", body: "Body of first article", user: @john)
+  end
+  
+  scenario "A non-signed in user does not see Edit or Delete links" do
+    visit "/"
+    
+    click_link @article.title
+    
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+     
+  end
+  
+  scenario "A non-owner signed in cannot see both links" do
+    login_as(@fred)
+    
+    visit "/"
+    
+    click_link @article.title
+    
+    expect(page).not_to have_link("Edit Article")
+    expect(page).not_to have_link("Delete Article")
+  end
+  
+  scenario "A signed in owner sees both links" do
+    login_as(@john)
+    
+    visit "/"
+    
+    click_link @article.title
+    
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
   end
   
   scenario "Display individual article" do
